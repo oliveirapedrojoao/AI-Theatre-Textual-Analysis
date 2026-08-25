0cc76a71b2d9cadb057733bbe0e96e084fe084dc
oliveirapedrojoao <116441214+oliveirapedrojoao@users.noreply.github.com>
2026-08-25 13:45:13 +0100

    Delete Web directory

--- Web/12-arranque.js
+++
@@ -1,4 +0,0 @@
-/* Ordem de arranque. Tem de ser o último ficheiro. */
-
-cabecalho(); tiles(); preencherFiltros(); ligarChrome(); prosa(); lerFiltros(); render();
-matchMedia("(prefers-color-scheme: dark)").addEventListener("change", render);
